6f81b16f6ab72386ff563e49b3fa4ecce85bd94b
alexey2140 <alexqac60m@gmail.com>
2022-08-17 21:49:58 +0300

    script_3.2.8.py, script_3.2.9.py are completed

--- stepik/2/script_3.2.8.py
+++ stepik/2/script_3.2.8.py
@@ -1,2 +1,12 @@
-assert abs(-42) == 42 # True assert
-assert abs(-42) == -42, "Should be absolute value of a number" # Assert with False
+#assert abs(-42) == 42 # True assert
+#assert abs(-42) == -42, "Should be absolute value of a number" # Assert with False
+
+def test_input_text(expected_result, actual_result):
+    # ваша реализация, напишите assert и сообщение об ошибке
+
+    num1 = expected_result
+
+    num2 = actual_result
+
+    assert num1 == num2, \
+        f"expected {num1}, got {num2}"
